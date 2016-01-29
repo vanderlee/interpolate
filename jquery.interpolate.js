9d8098e61209aaff15f5ae17400fc93a5b8349fd
Martijn W. van der Lee <martijn@vanderlee.com>
2016-01-29 21:29:49 +0100

    Chainable

--- jquery.interpolate.js
+++ jquery.interpolate.js
@@ -16,34 +16,39 @@
 	"use strict";
 
 	/**
-	 * Smoothly blend a CSS property with the value for the element
-	 *
 	 * .interpolate( propertyName, propertyValue[, blend[, easing]])
 	 * .interpolate( propertyMap[, blend[, easing]])
+	 *
+	 * Smoothly blend a CSS property with the value for the element
 	 *
 	 * @param {string} name Name of the property
 	 * @param {string} value Value of the property
 	 * @param {float} blend Ratio of blending in range [0,1] (default `0.5`)
 	 * @param {string} easing Easing method to use (default `linear`)
 	 */
-	$.fn.interpolate = function(name, value, blend, easing) {		
-		var _elem = this[0];
+	$.fn.interpolate = function(name, value, blend, easing) {
+		var elements = this;
 		if ($.isPlainObject(name)) {
 			easing = blend || 'linear';
 			blend = $.isNumeric(value) ? value : .5;
 			$.each(name, function(n, v) {
-				$.Tween(_elem, {duration: 1}, n, v, easing).run(blend);
+				$.each(elements, function() {
+					$.Tween(this, {duration: 1}, n, v, easing).run(blend);
+				});
 			});
 		} else {			
 			blend = $.isNumeric(blend) ? blend : .5;
-			$.Tween(_elem, {duration: 1}, name, value, easing || 'linear').run(blend);
+				$.each(elements, function() {
+					$.Tween(this[0], {duration: 1}, name, value, easing || 'linear').run(blend);
+				});
 		}
+		return this;
 	};
 
 	/**
-	 * Smoothly blend between two maps of CSS properties.
-	 *
 	 * jQuery.interpolate( propertyMapStart, propertyMapEnd[, blend[, easing]])
+	 *
+	 * Smoothly blend between two maps of CSS properties.
 	 *
 	 * @param {float} start Set of CSS properties to blend from
 	 * @param {object} end Set of CSS properties to blend into
